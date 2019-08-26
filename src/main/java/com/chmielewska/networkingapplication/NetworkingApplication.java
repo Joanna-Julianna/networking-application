58292b1c8c8f563c902a3ec827eb73b53fa2a8d8
Joanna Chmielewska <joanna.chmielewska@wipro.com>
2019-08-26 16:23:04 +0200

    Project skeleton

---
+++ src/main/java/com/chmielewska/networkingapplication/NetworkingApplication.java
@@ -0,0 +1,13 @@
+package com.chmielewska.networkingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkingApplication.class, args);
+    }
+
+}
